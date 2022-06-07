00b3f05937725e4b3f324198f756bb0e07efa758
Tom <tomrutenberg@mail.tau.ac.il>
2022-06-07 18:03:04 +0300

    wrote bm25_query function add normalization by query length in tfidf

--- vsm_ir.py
+++ vsm_ir.py
@@ -1,4 +1,5 @@
 # create_index command: python3 .\vsm_ir.py "create_index" ".\cfc-xml_corrected\"
+# TODO: after all small details and questions are answered should check option of creating auxiliary functions for common functionality between tfidf and bm25.
 
 import sys
 import os
@@ -8,13 +9,18 @@ import json
 import string
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from math import log, sqrt
 
 # ---------------------------------------- CONSTANTS ----------------------------------------
 CREATE_INDEX       = "create_index"
 QUESTION_ARGV      = "query"
 JSON_PATH          = "vsm_inverted_index.json"
 ANSWER_PATH        = "ranked_query_docs.txt"
-MAX_NUM_RESULTS    = 10 # TODO: need to find optimal value
+MAX_NUM_RESULTS    = 10     # TODO: need to find optimal value
+TFIDF_THRESHOLD    = 0.0    # TODO: need to find optimal value
+BM25_THRESHOLD     = 0.0    # TODO: need to find optimal value
+K_PARAM            = 1.2    # TODO: need to find optimal value
+B_PARAM            = 0.75   # TODO: need to find optimal value
 
 RECORD             = "RECORD"
 RECORDNUM          = "RECORDNUM"
@@ -208,6 +214,7 @@ def tfidf_query(path: str, query: str):
     clean_query = []                                # List containing all clean words in query
     query_dict = {}                                 # Dictionary with clean words as keys and tf-idf at values
     output_candidates = {}
+    query_length = 0.0
     query_words = query.strip().split()
     for word in query_words:
         clean_word = parseWord(word)
@@ -216,28 +223,59 @@ def tfidf_query(path: str, query: str):
     for clean_word in clean_query:
         if clean_word in q_words_dict.keys():
             query_dict[clean_word] = query_dict.get(clean_word, 0) + 1
-    # TODO: check if term frequency in query must be normalized by max frequency since we only care about relative order
+    # TODO: check if term frequency in query must be normalized by max frequency since in slide 76 in lecture notes it isn't
     max_frequency = max(query_dict.values())
     for term in query_dict:
         # calculate and updated tf * idf
         query_dict[term] = (query_dict[term] / max_frequency) * q_words_dict[term][IDF]
         for doc in q_words_dict[term][DOC_CONTAIN_WORD]:
             output_candidates[doc] = output_candidates.get(doc, 0.0) + query_dict[term] * q_docs_dict[doc][WORDS_IN_DOC][term][TF_IDF]
+        query_length += query_dict[term] ** 2
     for candidate in output_candidates:
-        output_candidates[candidate] /= q_docs_dict[candidate][VECTOR_LENGTH]
-    # TODO: check if normalization by query length is really necessary since we only care about relative order
+        output_candidates[candidate] /= q_docs_dict[candidate][VECTOR_LENGTH] * sqrt(query_length)
 
     # TODO: need to clean the finish of this method
     results = sorted(output_candidates.items(), key=lambda candidate: candidate[1], reverse=True)
-    clean_results = [result[0] for result in results]
+    clean_results = [result[0] for result in results if result[1] > TFIDF_THRESHOLD]
     output_result(clean_results[:MAX_NUM_RESULTS])
     return
 
 
 def bm25_query(path: str, query: str):
-    # TODO: need to complete this
-    
-    #output_result(clean_results[:MAX_NUM_RESULTS])
+    # TODO: what happens when same word has several occurences in the query - posted question in forum.
+    # TODO: the follow-up question is how to normalize the inner product <query, doc>
+    # current implementataion ignores multiple occurrences
+    inverted_index = json.load(open(path))
+    q_words_dict = inverted_index['words_dict']     
+    q_docs_dict = inverted_index['docs_dict']       
+    clean_query = []                                # List containing all clean words in query
+    query_dict = {}                                 # Dictionary with clean words as keys and tf-idf at values
+    output_candidates = {}
+    query_words = query.strip().split()
+    N = len(q_docs_dict.keys())                       # Number of documents
+    avgdl = 0.0
+    for doc in q_docs_dict.keys():
+        avgdl += q_docs_dict[doc][WORD_COUNT_IN_DOC]
+    avgdl = avgdl / N
+    for word in query_words:
+        clean_word = parseWord(word)
+        if clean_word is not None:
+            clean_query.append(clean_word)
+    for clean_word in clean_query:
+        if clean_word in q_words_dict.keys():
+            query_dict[clean_word] = query_dict.get(clean_word, 0) + 1
+    for term in query_dict:
+        count = len(q_words_dict[term][DOC_CONTAIN_WORD].keys())        # num of docs containing the term
+        # calculate the bm25 idf according go slide 39 in lecture 4 slides
+        idf = log( (N - count + 0.5) / (count + 0.5) + 1)
+        for doc in q_words_dict[term][DOC_CONTAIN_WORD]:
+            term_count = q_words_dict[term][DOC_CONTAIN_WORD][doc][COUNT_WORD_IN_DOC]
+            bm25_weight = (term_count * (K_PARAM + 1)) / (term_count + K_PARAM * (1 - B_PARAM + B_PARAM * q_docs_dict[doc][WORD_COUNT_IN_DOC] / avgdl))
+            output_candidates[doc] = output_candidates.get(doc, 0.0) + idf * bm25_weight
+    results = sorted(output_candidates.items(), key=lambda candidate: candidate[1], reverse=True)
+    clean_results = [result[0] for result in results if result[1] > BM25_THRESHOLD]
+    output_result(clean_results[:MAX_NUM_RESULTS])
+    return
 
 
 if __name__ == '__main__':
